e19f41d5e5fb34dba2446cbfe4a796feb7d084ee
ToshihitoKon <shiratori991@gmail.com>
2016-03-22 05:51:19 +0900

    HP ver.0.9

--- script.js
+++ script.js
@@ -6,23 +6,41 @@ $(function(){
     function(){
         $(this).css("background","rgba(0,0,0,0)")
     });
-});
-$(function(){
-    $(".btn_reg").hover(
+	$(".title").click(function(){
+		location.href = "index.html";
+	});
+
+    $(".btn_lnk").hover(
     function(){
         $(this).css("background","rgba(80,80,80,0.9)")
     },
     function(){
         $(this).css("background","rgba(0,0,0,0)")
     });
-});
-$(function(){
-    $(".btn_sta").hover(
+	$(".btn_lnk").click(function(){
+		location.href = "link.html";
+	});
+
+    $(".btn_wok").hover(
     function(){
         $(this).css("background","rgba(80,80,80,0.9)")
     },
     function(){
         $(this).css("background","rgba(0,0,0,0)")
     });
+	$(".btn_wok").click(function(){
+		location.href = "works.html";
+	});
+
+	$(".btn_abt").hover(
+    function(){
+        $(this).css("background","rgba(80,80,80,0.9)")
+    },
+    function(){
+        $(this).css("background","rgba(0,0,0,0)")
+    });
+	$(".btn_abt").click(function(){
+		location.href = "about.html";
+	});
 });
 
